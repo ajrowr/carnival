a134163e279871a963b78494c4d48314ef580a52
Alan Rowarth <alan@codex.cx>
2017-01-24 16:23:49 -0800

    Improvements to color handling Added raft-follow player movement paradigm

--- carnival.js
+++ carnival.js
@@ -73,7 +73,7 @@ window.FCComponent = (function () {
         this.drawParams = {
             materialLabel: d.materialLabel || 'matteplastic',
             textureLabel: d.textureLabel || null,
-            texture: d.texture || null,
+            texture: d.texture || (d.color && CARNIVAL.color(d.color).asTexture()) || null,
             shaderLabel: d.shaderLabel || null,
             position: d.position || {x:0, y:1, z:0},
             orientation: d.orientation || {x:0, y:0, z:0},
@@ -377,7 +377,15 @@ window.CARNIVAL = (function () {
             
         };
         
-        this.color = FCColor;
+        // this.color = FCColor;
+        this.color = function (designator) {
+            if (designator.startsWith('#')) {
+                return new FCColor(designator);
+            }
+            else {
+                return this.colors[designator];
+            }
+        }
         this.colors = {};
         [['indigo', '#4b0082'], ['gold', '#ffd700'], ['hotpink', '#ff69b4'], ['firebrick', '#b22222'], ['lightseagreen', '#20b2aa'], ['yellow', '#ffff00'], ['mistyrose', '#ffe4e1'], ['darkolivegreen', '#556b2f'], ['darkseagreen', '#8fbc8f'], ['pink', '#ffc0cb'], ['lightcoral', '#f08080'], ['orangered', '#ff4500'], ['navajowhite', '#ffdead'], ['lime', '#00ff00'], ['palegreen', '#98fb98'], ['greenyellow', '#adff2f'], ['burlywood', '#deb887'], ['seashell', '#fff5ee'], ['mediumspringgreen', '#00fa9a'], ['fuchsia', '#ff00ff'], ['papayawhip', '#ffefd5'], ['blanchedalmond', '#ffebcd'], ['chartreuse', '#7fff00'], ['dimgray', '#696969'], ['black', '#000000'], ['peachpuff', '#ffdab9'], ['springgreen', '#00ff7f'], ['aquamarine', '#7fffd4'], ['white', '#ffffff'], ['orange', '#ffa500'], ['lightsalmon', '#ffa07a'], ['darkslategray', '#2f4f4f'], ['brown', '#a52a2a'], ['ivory', '#fffff0'], ['dodgerblue', '#1e90ff'], ['peru', '#cd853f'], ['lawngreen', '#7cfc00'], ['chocolate', '#d2691e'], ['crimson', '#dc143c'], ['forestgreen', '#228b22'], ['slateblue', '#6a5acd'], ['olive', '#808000'], ['cyan', '#00ffff'], ['mintcream', '#f5fffa'], ['silver', '#c0c0c0'], ['antiquewhite', '#faebd7'], ['mediumorchid', '#ba55d3'], ['skyblue', '#87ceeb'], ['gray', '#808080'], ['darkturquoise', '#00ced1'], ['goldenrod', '#daa520'], ['darkgreen', '#006400'], ['floralwhite', '#fffaf0'], ['darkviolet', '#9400d3'], ['darkgray', '#a9a9a9'], ['moccasin', '#ffe4b5'], ['saddlebrown', '#8b4513'], ['darkslateblue', '#483d8b'], ['lightskyblue', '#87cefa'], ['lightpink', '#ffb6c1'], ['mediumvioletred', '#c71585'], ['red', '#ff0000'], ['deeppink', '#ff1493'], ['limegreen', '#32cd32'], ['darkmagenta', '#8b008b'], ['palegoldenrod', '#eee8aa'], ['plum', '#dda0dd'], ['turquoise', '#40e0d0'], ['lightgrey', '#d3d3d3'], ['lightgoldenrodyellow', '#fafad2'], ['darkgoldenrod', '#b8860b'], ['lavender', '#e6e6fa'], ['maroon', '#800000'], ['yellowgreen', '#9acd32'], ['sandybrown', '#f4a460'], ['thistle', '#d8bfd8'], ['violet', '#ee82ee'], ['navy', '#000080'], ['magenta', '#ff00ff'], ['tan', '#d2b48c'], ['rosybrown', '#bc8f8f'], ['olivedrab', '#6b8e23'], ['blue', '#0000ff'], ['lightblue', '#add8e6'], ['ghostwhite', '#f8f8ff'], ['honeydew', '#f0fff0'], ['cornflowerblue', '#6495ed'], ['linen', '#faf0e6'], ['darkblue', '#00008b'], ['powderblue', '#b0e0e6'], ['seagreen', '#2e8b57'], ['darkkhaki', '#bdb76b'], ['indianred ', '#cd5c5c'], ['snow', '#fffafa'], ['sienna', '#a0522d'], ['mediumblue', '#0000cd'], ['royalblue', '#4169e1'], ['lightcyan', '#e0ffff'], ['green', '#008000'], ['mediumpurple', '#9370d8'], ['midnightblue', '#191970'], ['cornsilk', '#fff8dc'], ['paleturquoise', '#afeeee'], ['bisque', '#ffe4c4'], ['slategray', '#708090'], ['darkcyan', '#008b8b'], ['khaki', '#f0e68c'], ['wheat', '#f5deb3'], ['teal', '#008080'], ['darkorchid', '#9932cc'], ['salmon', '#fa8072'], ['deepskyblue', '#00bfff'], ['rebeccapurple', '#663399'], ['darkred', '#8b0000'], ['steelblue', '#4682b4'], ['palevioletred', '#d87093'], ['lightslategray', '#778899'], ['aliceblue', '#f0f8ff'], ['lightgreen', '#90ee90'], ['orchid', '#da70d6'], ['gainsboro', '#dcdcdc'], ['mediumseagreen', '#3cb371'], ['tomato', '#ff6347'], ['mediumturquoise', '#48d1cc'], ['lemonchiffon', '#fffacd'], ['cadetblue', '#5f9ea0'], ['lightyellow', '#ffffe0'], ['lavenderblush', '#fff0f5'], ['coral', '#ff7f50'], ['purple', '#800080'], ['aqua', '#00ffff'], ['whitesmoke', '#f5f5f5'], ['mediumslateblue', '#7b68ee'], ['darkorange', '#ff8c00'], ['mediumaquamarine', '#66cdaa'], ['darksalmon', '#e9967a'], ['beige', '#f5f5dc'], ['blueviolet', '#8a2be2'], ['azure', '#f0ffff'], ['lightsteelblue', '#b0c4de'], ['oldlace', '#fdf5e6']]
         .forEach(s => this.colors[s[0]] = new FCColor(s[1]));
